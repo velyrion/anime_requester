8a9cf4139bbc1e8cd8d3291743481546288f77b8
Victor Paris Lemperrière <vic.pl930@gmail.com>
2025-09-29 09:52:18 +0200

    [FEATURE] getAnime

---
+++ scripts/animeApi.js
@@ -0,0 +1,51 @@
+/*API VARIABLES*/
+const apiKey = 'a98487f17emshf797434733b889cp124f03jsn461d3ecc651c';
+const url = 'https://anime-db.p.rapidapi.com/anime?page=1';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': apiKey,
+		'x-rapidapi-host': 'anime-db.p.rapidapi.com'
+	}
+};
+/*END OF API VARIABLES*/
+
+/*HTML VARIABLES*/
+const cards = document.getElementById("cards");
+/*END OF HTML VARIABLES*/
+
+/*FONCTIONS*/
+function getUrl(search='', size='10', genre='', sortBy='', sortOrder='') {
+    if (search != '') search = '&search='+search;
+    if (size != '') size = '&size='+size;
+    if (genre != '') genre = '&genres='+genre;
+    if (sortBy != '') sortBy = '&sortBy='+sortBy;
+    if (sortOrder != '') sortOrder = '&sortOrder='+sortOrder;
+    return url+size+search+genre+sortBy+sortOrder;
+}
+
+async function getAnime() {
+    try {
+        const response = await fetch(getUrl(), options);
+        const result = await response.json();
+        return result.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function addCards(animes) {
+    animes.forEach(anime => {
+        cards.innerHTML += "<div class='card'> \
+            <h3>${anime.title}</h3> \
+            <img src=${'"+anime.image+"'} alt=${'"+anime.title+"'} /> \
+            <p>${anime.synopsis}</p> \
+          </div>";
+    });
+}
+
+/*END OF FONCTIONS*/
+
+getAnime();
+
+//addCards(getAnime());
